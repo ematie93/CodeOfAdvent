4ed5f7e2ec06e0a61d5e0174291bdd5761614c28
mateo.mtg <mateo.mtg@gmail.com>
2021-12-20 20:22:24 +0100

    exercise 2 done correctly

--- 02/main.py
+++ 02/main.py
@@ -30,14 +30,39 @@ def deepDive(orderList, order = 0, x = 0, y = 0):
         if argument == "forward":
             x = x + int(step[1])
         elif argument == "up":
-            y = y + int(step[1])
-        else:
             y = y - int(step[1])
+        else:
+            y = y + int(step[1])
 
         print(str(x) + " " + str(y))
 
         return deepDive(mylist, order + 1, x, y)
         
+def deepDive2(orderList, order = 0, x = 0, y = 0, aim = 0):
 
-deepDive(mylist)
+    orderLen = len(orderList)
+    #lastItemIndex = orderList.index(orderList[orderLen])
+    
+    if int(order) == int(orderLen):
+        print("No more data")
+        print("The Final Position is: " + str(x) + ","+ str(y) + ". Submit is " + str(x*y))
+    
+    else:
+        step = orderList[order].split()
+        argument = step[0]
+
+        if argument == "up":
+            aim = aim - int(step[1])
+        elif argument == "down":
+            aim = aim + int(step[1])
+        else:
+            x = x + int(step[1])
+            y = aim * int(step[1]) + y
+
+        print(str(step[1]) + " * " + str(aim) + " = " + str(y))
+
+        return deepDive2(mylist, order + 1, x, y, aim)
+
+
+deepDive2(mylist)
 
