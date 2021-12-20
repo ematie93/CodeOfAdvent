1664455acf9e07f76197c68ffef2284c293f79d4
mateo.mtg <mateo.mtg@gmail.com>
2021-12-20 19:00:17 +0100

    .

--- 01/main.py
+++ 01/main.py
@@ -13,17 +13,29 @@ with open('puzzle.txt') as f:
 ######### ITS SOOOOOO IMPORTANT TO CONVERT IT TO INTS, STR COMPARASION COMPARES UNICODE BYTS!!!!!!!!!!!
 
 limit = len(mylist)
+mylist2 = []
 countInc = 0
+
 
 #### Part A
 for i in range(1, (limit)):
     
     if int(mylist[i - 1]) <= int(mylist[i]):
-        print(str(mylist[i-1])+ " < " +  mylist[i])
         countInc += 1
-    else:
-        print(str(mylist[i-1])+ " > " +  mylist[i])
-
 
 ##### Part B
-print(countInc)
+#create new 
+for i in range(0, (limit - 1)):
+    if len(mylist[i:i+3]) == 3:
+        suma = sum(mylist[i:i+3])
+        mylist2.append(suma)
+limit2 = len(mylist2)
+countInc2 = 0
+
+for i in range(1, (limit2)):
+    
+    if int(mylist2[i - 1]) < int(mylist2[i]):
+        countInc2 += 1
+  
+print(countInc2)
+
